3c9d78db45921163505663acce3964f17396f857
Akash Singh <akashsinghas9504@gmail.com>
2022-09-28 14:20:17 +0530

    Addition

---
+++ add.js
@@ -0,0 +1,10 @@
+function add()
+{
+
+var num1=document.getElementById("firstno").value
+var num2=document.getElementById("secondno").value
+
+var result= num1+num2
+
+document.getElementById("result").value=result
+}
