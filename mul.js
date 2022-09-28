d09428cd38a12da5f323d43c7d2205cf9b75c112
Akash Singh <akashsinghas9504@gmail.com>
2022-09-28 15:47:43 +0530

    multiplication added

---
+++ mul.js
@@ -0,0 +1,7 @@
+function mul()
+{
+var num1=document.getElementById("firstno").value
+var num2=document.getElementById("secondno").value
+var result=parseInt(num1)*parseInt(num2)
+document.getElementById("result").value=result
+}
