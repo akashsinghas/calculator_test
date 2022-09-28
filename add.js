3f4441d44c1f16a6e88331e4ccce590cc2e69707
Akash Singh <akashsinghas9504@gmail.com>
2022-09-28 15:08:40 +0530

    Add

--- add.js
+++ add.js
@@ -1,16 +1,7 @@
 function add()
 {
-<<<<<<< HEAD
-
-var num1=document.getElementById("firstno").value
-var num2=document.getElementById("secondno").value
-
-var result= num1+num2
-
-=======
 var num1=document.getElementById("firstno").value
 var num2=document.getElementById("secondno").value
 var result=parseInt(num1)+parseInt(num2)
->>>>>>> addition
 document.getElementById("result").value=result
 }
